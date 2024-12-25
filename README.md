7f8e55905e6f8cda9abf11d975cec8920eb7ea87
Tamim-Rizvee <tamimulislan@gmail.com>
2024-12-25 12:23:56 +0600

    Added a new fie and changed the readme file

--- README.md
+++ README.md
@@ -2,3 +2,6 @@
 Hello! This is a trial repository of mine :)
 <br>
 Author: Md Tamimul Islam RIzvee
+<br>
+I am from MIST 
+
---
+++ hello.cpp
@@ -0,0 +1,11 @@
+#include<bits/stdc++.h>
+#define Onii_chan ios_base::sync_with_stdio(0),cin.tie(0),cout.tie(0)
+#define iint long long
+#define uwu '\n'
+using namespace std;
+int main()
+{
+    Onii_chan;
+    cout << "Testing for the github" << uwu;
+    return 0;
+}
